ffd0cc54670431c7e506f70c39e76b6c1f55b3db
mdnazimmia <mdnazimmia2@gmail.com>
2023-03-15 23:41:48 +0100

    variable type

--- variable.js
+++ variable.js
@@ -1,7 +1,19 @@
 // vary + able = variable 
+
+// numeric
 var price = 11;
 var age = 18;
 var temperature = 32;
 var weight = 74;
 var mony = 500;
 var marks = 90;
+
+// string
+var person = "nazim mia";
+var location = "Mülheim an der Rurh";
+var special = "Sauda Akter";
+
+// boolean
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
