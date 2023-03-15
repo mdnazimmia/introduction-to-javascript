d2cba4ab9f4d89a792c7a337f008db1230f7cc7a
mdnazimmia <mdnazimmia2@gmail.com>
2023-03-15 23:57:31 +0100

    keyword, variable name meaning convention

---
+++ variable-name.js
@@ -0,0 +1,24 @@
+// good variable
+var price = 29;
+// 1. variable name can not be any keywords
+var false = 96;
+var retur4n = true;
+
+// 2. variabl name has to be in one work. No space
+var my home address = "Mülheim an der Ruhr"
+
+// 3. variable name can not have quotation
+var "name" = "Tom Hanks";
+
+// 4. variable name can not starts with a number but can ends with a number
+var 99Club = 1964;
+var club25 = 2025;
+
+// 5. How to use long names
+var user-name = "raj bappa";
+
+var user_name = "bapparaj";
+var userhomeaddress = "andor killa bandor ban";
+var user_home_address = "andor killa bandor ban"; //snake case
+var userHomeAddress = "andor killa bandor ban"; //camel case : we will use this one
+var UserHomeAddress = "andor killa bandor ban"; //pascal case
