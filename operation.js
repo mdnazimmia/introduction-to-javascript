05519ccceb48b55189d9f672a11413509797816f
mdnazimmia <mdnazimmia2@gmail.com>
2023-03-19 01:10:58 +0100

    four operaiton done

--- operation.js
+++ operation.js
@@ -2,8 +2,23 @@ var onionPrice = 20;
 var eggPrice = 10;
 
 var totalPrice = (onionPrice + eggPrice);
-console.log(totalPrice);
 
+var priceDifference = onionPrice - eggPrice;
+console.log(priceDifference);
+
+// multiplication
+var orangePrice = 20;
+var quantiy = 7;
+var totalCost = orangePrice * quantiy;
+console.log(totalCost);
+
+// division
+var money = 600;
+var player = 12;
+var eachPlayer = money / player;
+console.log(eachPlayer);
+
+// console.log(totalPrice);
 // console.log(onionPrice);
 // console.log(eggPrice);
 // console.log(onionPrice + eggPrice);
