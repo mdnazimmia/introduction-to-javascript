5cd20d32df04287fb2db885128cf97e95fc18991
mdnazimmia <mdnazimmia2@gmail.com>
2023-03-21 00:08:37 +0100

    add new number

--- shorthand.js
+++ shorthand.js
@@ -3,3 +3,11 @@ var tomatoPrice = 25;
 
 // variable price change
 tomatoPrice = 30;
+
+// add some value to the previous value
+price1 = 31;
+price2 = 14;
+
+// add some value to the existing value
+price1 = price1 + 14;
+console.log(price1)
