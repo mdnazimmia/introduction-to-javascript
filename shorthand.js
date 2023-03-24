24e2562aa5f3f589460640ae856d850dc3779bb0
mdnazimmia <mdnazimmia2@gmail.com>
2023-03-24 23:18:18 +0100

    short hand added

--- shorthand.js
+++ shorthand.js
@@ -10,8 +10,17 @@ price2 = 14;
 
 // add some value to the existing value
 price1 = price1 + 14;
-console.log(price1)
+// console.log(price1)
 
 // addition to the same variable shorthand
 // += means adding some value to the existing value
 price1 += 10;
+
+price2 = price2 - 5;
+// shorthand -=
+// -= means deducting some values form the current value of the variable
+price2 -= 5;
+
+// *=
+price1 *= 5;
+
