73d728d2a2f5248363e40caaa0fa8a5e9c2ef1a9
mdnazimmia <mdnazimmia2@gmail.com>
2023-03-28 22:49:51 +0200

    cahnging the value of a variable after a mathematical operaiton

--- shorthand.js
+++ shorthand.js
@@ -8,6 +8,9 @@ tomatoPrice = 30;
 price1 = 31;
 price2 = 14;
 
+// set a new value
+// price1 = 35;
+
 // add some value to the existing value
 price1 = price1 + 14;
 // console.log(price1)
@@ -23,10 +26,19 @@ price2 -= 5;
 
 // *=
 price1 *= 5;
+console.log(price1)
 
-// shorthand means deducting some values form the current value of the variable
-var carPrice = 300;
-var instrumentPrice = 100;
+// /=5;
+price1 /= 5;
+consol.log(price1)
 
-carPrice += 100;
-console.log(carPrice)
+var age = 15;
+age = age + 1;
+//  ++ means adding 1 to the existing
+age++;
+
+var love = 100;
+lvoe = love - 1;
+// -- means you are deducting 1 from the existing value
+love--;
+
