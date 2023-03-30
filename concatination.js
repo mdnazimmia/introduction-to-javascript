c5a749155a7dbd10baf291eef1c551f798201888
mdnazimmia <mdnazimmia2@gmail.com>
2023-03-30 23:38:48 +0200

    concatination

---
+++ concatination.js
@@ -0,0 +1,9 @@
+var price1 = 30;
+var price2 = 10;
+var totalprice = price1 + price2;
+// console.log(totalprice);
+
+var firstName = 'Nur Ibnul';
+var lastName = 'Arabi';
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
--- conversion.js
+++
@@ -1,4 +0,0 @@
-var price1 = 30;
-var price2 = 10;
-var totalprice = price1 + price2;
-console.log(totalprice);
