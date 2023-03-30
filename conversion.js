476cc699d0ddfe1ee541a6ede45991b2d5d79e52
mdnazimmia <mdnazimmia2@gmail.com>
2023-03-31 00:13:03 +0200

    conversion.js

--- conversion.js
+++ conversion.js
@@ -7,3 +7,13 @@ var price1 = '31';
 var price2 = 10;
 var totalPrice = price1 + price2;
 console.log(totalPrice)
+
+// convert string to number
+console.log(price2);
+var price1Number = parseInt(price1);
+console.log(price1Number);
+// 
+
+var gpa = '2.25';
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber);
