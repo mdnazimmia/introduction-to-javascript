54e19b853681add692d216b97a53ca2d21110290
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-01 23:06:04 +0200

    firstproblem

---
+++ practiceproblem-1.js
@@ -0,0 +1,7 @@
+var momGiven = 1000;
+var oranges = 400;
+var apple = 300;
+var totalPrice = oranges + apple;
+// console.log(totalPrice)
+var shopkepperReturn = momGiven - totalPrice;
+console.log(shopkepperReturn)
