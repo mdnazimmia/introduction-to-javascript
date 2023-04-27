939294fbee30f844a9cf3a48c1483908cc871a06
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-27 10:05:51 +0200

    new practice

---
+++ practice.js
@@ -0,0 +1,22 @@
+// var name = 50;
+// var location = 'Mülheim an der Rurh';
+// var serioius = true;
+
+var eggPrice = 30;
+var floorPrice = 40;
+var totalPrice = (eggPrice + floorPrice);
+console.log(totalPrice);
+// console.log(eggPrice);
+// console.log(floorPrice);
+var priceDifference = (eggPrice - floorPrice);
+// console.log(priceDifference);
+
+var dailyCoast = 100;
+var totalParticipate = 14;
+var totalCoast = (dailyCoast * totalParticipate);
+// console.log(totalCoast);
+
+var budget = 10000;
+var totalPerson = 5;
+var budget = (budget / totalPerson);
+console.log(budget);
