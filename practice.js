db0ea745c88e6c4f1baccf82a9884dc0de93b722
mdnazimmia <mdnazimmia2@gmail.com>
2023-04-28 10:25:26 +0200

    mathematical operation

--- practice.js
+++ practice.js
@@ -1,22 +1,61 @@
-// var name = 50;
-// var location = 'Mülheim an der Rurh';
-// var serioius = true;
+// // var name = 50;
+// // var location = 'Mülheim an der Rurh';
+// // var serioius = true;
 
-var eggPrice = 30;
-var floorPrice = 40;
-var totalPrice = (eggPrice + floorPrice);
+// var eggPrice = 30;
+// var floorPrice = 40;
+// var totalPrice = (eggPrice + floorPrice);
+// console.log(totalPrice);
+// // console.log(eggPrice);
+// // console.log(floorPrice);
+// console.log
+// var priceDifference = (eggPrice - floorPrice);
+// // console.log(priceDifference);
+
+// var dailyCoast = 100;
+// var totalParticipate = 14;
+// var totalCoast = (dailyCoast * totalParticipate);
+// // console.log(totalCoast);
+
+// var budget = 10000;
+// var totalPerson = 5;
+// var budget = (budget / totalPerson);
+// // console.log(budget);
+
+// var applePrice = 300;
+// var guabaPrice = 200;
+// var totalPrice = (applePrice + guabaPrice);
+// // console.log(totalPrice);
+
+// var daylyCoast = 500;
+// var totalDay = 30;
+// var totalCoast = (500 * 30);
+// // console.log(totalCoast);
+
+// var proPerson = (500 / 30);
+// // console.log(proPerson);
+
+var applePrice = 30;
+var annanasPrice = 70;
+var totalPrice = (applePrice + annanasPrice);
+// console.log(totalPrice);
+
+// applePrice = applePrice + 20;
+applePrice += 50;
+console.log(applePrice);
+
+applePrice -= 20;
+console.log(applePrice);
 console.log(totalPrice);
-// console.log(eggPrice);
-// console.log(floorPrice);
-var priceDifference = (eggPrice - floorPrice);
-// console.log(priceDifference);
 
-var dailyCoast = 100;
-var totalParticipate = 14;
-var totalCoast = (dailyCoast * totalParticipate);
-// console.log(totalCoast);
 
-var budget = 10000;
-var totalPerson = 5;
-var budget = (budget / totalPerson);
-console.log(budget);
+applePrice *= 6;
+console.log(applePrice);
+
+applePrice /= 10;
+console.log(applePrice);
+
+applePrice++;
+console.log(applePrice);
+applePrice--;
+console.log(applePrice);
